4d2a1c44537c4070a22d60b8b2d3044a9dfb8cfd
Rifki K <iamrifki0@gmail.com>
2020-05-17 02:50:09 +0700

    Publish to GitHub

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# brexit
+# Brexit
+
 Get ready for [blank], inspired by @readyforspoons.
+
+Code is based on [getreadyfor](https://github.com/cool-robot-pals/getreadyfor).
+
+## Usage
+
+Add the word that you want at the end of the url like so:
+
+`iamrifki.github.io/brexit?text=memes`
+
+![One](./assets/1.png)
+
+You can even do sentences with spaces in it.
+
+`iamrifki.github.io/brexit?text=text adventure`
+
+![Two](./assets/2.png)
---
+++ js/js.js
@@ -0,0 +1,16 @@
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const text = urlParams.get("text") === null ? "blank" : urlParams.get("text") ;
+
+window.onload = function () {
+  const span = document.querySelector("span");
+  const matches = document.querySelectorAll("x-word");
+  for (const index in matches) {
+    if (matches[index].textContent === "blank") {
+      matches[index].textContent = text;
+      if (span) {
+        span.textContent = text.replace(/\s/g , "-");
+      }
+    }
+  }
+};
