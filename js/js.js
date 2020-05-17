1198238384c6b86bf3665babf7eccdf4bb284760
Rifki K <iamrifki0@gmail.com>
2020-05-17 08:52:36 +0700

    2 Updates. - Add Unicode normalization for the fake gov.uk link. - Add mention of @Matojeje's Discord bot command.

--- js/js.js
+++ js/js.js
@@ -5,11 +5,15 @@ const text = urlParams.get("text") === null ? "blank" : urlParams.get("text") ;
 window.onload = function () {
   const span = document.querySelector("span");
   const matches = document.querySelectorAll("x-word");
+
   for (const index in matches) {
     if (matches[index].textContent === "blank") {
       matches[index].textContent = text;
       if (span) {
-        span.textContent = text.replace(/\s/g , "-");
+        span.textContent = text
+                            .replace(/\s/g , "-")
+                            .normalize("NFKD")
+                            .replace(/[\u0300-\u036F]/g, "");
       }
     }
   }
